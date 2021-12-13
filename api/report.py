51d71d7e00d735d2e2b9626dc16ae7cfd918514f
user <user@BFG-548>
2021-12-13 16:42:19 +0400

    Метод для текущей локации.

--- api/report.py
+++ api/report.py
@@ -2,7 +2,7 @@ from flask_apispec import marshal_with, doc
 
 from AlexeyService.reports.batch import create_report_on_batch_by_filters
 from app import *
-from test_data.python_data import analytics
+from test_data.python_data import analytics, in_out_events, current_location
 from utils import responses as resp
 from utils.responses import response_with
 
@@ -32,3 +32,17 @@ def get_analytics(id):
 
 
 docs.register(get_analytics)
+
+
+@app.route('/api/currentlocation/<int:id>/', methods=['GET'], provide_automatic_options=False)
+@doc(description='Получить текущее местоположение партии.', tags=['report'])
+@resp.check_user_permission(dbName="BatchDB", method='GET')
+def get_current_location(id):
+    """
+    Если на станке, тогда массив из id зоны, если в пути, тогда из 2-х id.
+    """
+    query = current_location[id-1]
+    return response_with(resp.SUCCESS_200, value={"query": json.dumps(query)})
+
+
+docs.register(get_report_batch_movement)
